606effcedec85a022e5aaad25c429c3eff43f57b
Scott <marden.scott@gmail.com>
2017-07-13 11:54:38 -0700

    Initial commit. Completed assignment. Generates random barcode of colors based on provided array which is randomized.

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	title = 'Retro Barcode Generator';
+	colors: Array<string> = ['red', 'green', 'blue', 'seagreen', 'aqua', 'blueViolet', 'purple', 'DarkRed']
+
+	shuffleArr(arr){
+		let currentIndex = arr.length, temporaryValue, randomIndex;
+
+		while(0 !== currentIndex){
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			temporaryValue = arr[currentIndex];
+			arr[currentIndex] = arr[randomIndex];
+			arr[randomIndex] = temporaryValue;
+		}
+	}
+
+	randColors = this.shuffleArr(this.colors);
+}
